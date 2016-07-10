c143500c363b4313b598ee376bdff2853a2e52fa
Dylan T. Moberg <dylanmoberg@msn.com>
2016-07-09 19:34:18 -0700

    initial commit and chapter 3 code

---
+++ chapter3.js
@@ -0,0 +1,39 @@
+(function() {
+'use strict';
+function minimum(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
+function whyRecurse(num) {
+  num = Math.abs(num);
+  if (num === 0) {
+    return true;
+  }
+  if (num === 1) {
+    return false;
+  }
+  num -= 2;
+  return whyRecurse(num);
+}
+
+
+
+
+function countBs(string, letter) {
+  var counter = 0;
+  for (var i = 0; i < string.length; i++) {
+    if(string[i] === letter) {
+      counter ++;
+    }
+  }
+ return(`there are ${counter} ${letter}\'s`);
+}
+
+console.log(countBs('babadook','b'));
+
+})();
