63c9aa418b27932e5832356bbe43700c84fb4c30
Dylan T. Moberg <dylanmoberg@msn.com>
2016-07-10 02:11:21 -0700

    almost done with chapter 4, recursive object constructor

--- chapter4.js
+++ chapter4.js
@@ -6,28 +6,93 @@
 
 //step is always a positive value
 
-function myRange (start,stop,step) {
-    var myArr = [];
-    var accending = 1;
-    var increasing = 1;
-    if (start > step) {
-        accending = -1;
-    }
-    if (step < 0) {
-        increasing = -1;
-    }
-    for (var i = start; accending*i <= stop; i+=(step*(accending))) {
-        myArr.push(i);
-
-}
-    return myArr;
-}
-
+// function myRange(start, stop, step) {
+//     var myArr = [];
+//     var accending = 1;
+//     var increasing = 1;
+//     if (start > step) {
+//         accending = -1;
+//     }
+//     if (step < 0) {
+//         increasing = -1;
+//     }
+//     for (var i = start; accending * i <= stop; i += (step * (accending))) {
+//         myArr.push(i);
+//
+//     }
+//     return myArr;
+// }
+//
 // function sum(array) {
-//   return array.reduce(function(a,b) {
-//     return a + b;
-//   });
+//     return array.reduce(function(a, b) {
+//         return a + b;
+//     });
 // }
 
-console.log(myRange(1,10,2));
-console.log(myRange(100, -5,2));
+
+// Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as an argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+//
+// Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+// function reverseArray(array) {
+//     var newArray = [];
+//     array.map(function(element) {
+//         newArray.unshift(element);
+//     });
+//     return newArray;
+// }
+//
+// function reverseArrayInPlace(array) {
+//     for (var i = 0; i < Math.floor(array.length / 2); i++) {
+//         var old = array[i];
+//         array[i] = array[array.length - 1 - i];
+//         array[array.length - 1 - i] = old;
+//     }
+//     return array;
+// }
+
+// A List
+// Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with the array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+//
+
+// The resulting objects form a chain, like this:
+
+// A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the variable defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. In addition, the original list is also still a valid three-element list.
+//
+// Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as an argument, and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list, or undefined when there is no such element.
+//
+// If you haven’t already, also write a recursive version of nth.
+
+
+
+// function arrayToList(array) {
+//     if()
+//
+// }
+var list = {
+    value: 1,
+    rest: {
+        value: 2,
+        rest: {
+            value: 3,
+            rest: null
+        }
+    }
+};
+var testArray = [1,2,3,4,5];
+var obj = {};
+function objConstructor(array, obj, currentObj) {
+
+    if (array.length === 0) {
+        console.log(obj);
+        return obj;
+    }
+    if (! currentObj.hasOwnProperty('value')) {
+        currentObj.value = array.shift();
+        currentObj.rest = {};
+        return objConstructor(array, obj, obj.rest);
+    }
+
+}
+
+ console.log(objConstructor([1, 2, 3, 4, 5], {}));
