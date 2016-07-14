3f38aa479609bd5b25635a306a9f3b9f20e11bf4
Dylan Moberg <dylanmoberg11@gmail.com>
2016-07-14 15:15:28 -0700

    first two questions finished in chapter 5

--- chapter5.js
+++ chapter5.js
@@ -181,18 +181,15 @@ console.log(arrays.reduce(function(a,b) {
 //
 // Note that not all the mothers mentioned in the data are themselves present in the array. The byName object, which makes it easy to find a person’s object from their name, might be useful here.
 
-function average(array) {
-  function plus(a, b) { return a + b; }
-  return array.reduce(plus) / array.length;
-}
-
 var byName = {};
 ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-
-console.log(byName);
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
 
 function ageDifference(obj, person, mother) {
     mother = obj[mother];
@@ -201,7 +198,6 @@ function ageDifference(obj, person, mother) {
     } else {
         return person.born - mother.born;
     }
-
 }
 
 function totalDifferences(obj) {
@@ -210,11 +206,33 @@ function totalDifferences(obj) {
         var curr = obj[key];
         diffArray.push(ageDifference(obj, curr, curr.mother));
     }
-    return diffArray;
+    return diffArray.filter(function(value) {
+        return value > 0;
+    });
 }
 
-console.log(totalDifferences(byName));
+console.log(average(totalDifferences(byName)));
 
-console.log('teset');
+
+// Historical life expectancy
+//
+// When we looked up all the people in our data set that lived more than 90 years, only the latest generation in the data came out. Let’s take a closer look at that phenomenon.
+//
+// Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 } )();
