ffb68f3d348490880a70101b3081ae0dd17568ff
Dylan Moberg <dylanmoberg@LexBlogs-MacBook-Pro-4.local>
2016-07-13 17:55:59 -0700

    gotta go play mini golf

--- chapter5.js
+++ chapter5.js
@@ -66,16 +66,27 @@ var ancestry = JSON.parse(ANCESTRY_FILE);
 //
 // console.log(ancestry);
 
-function reduce(array,combine,start) {
-    var current = start;
-    for (var i = 0; i < array.length; i++) {
-        current = combine(current, array[i]);
-    }
-    return current;
-}
+// function reduce(array,combine,start) {
+//     var current = start;
+//     for (var i = 0; i < array.length; i++) {
+//         current = combine(current, array[i]);
+//     }
+//     return current;
+// }
+//
+// console.log(reduce([1, 2, 3, 4], function(a,b) {
+//     return a + b;
+// }, 0));
 
-console.log(reduce([1, 2, 3, 4], function(a,b) {
-    return a + b;
-}, 0));
+console.log(ancestry.reduce(function(min,cur) {
+    if (cur.born < min.born) {
+        return cur;
+    } else {
+        return min;
+    }
+}));
+
+
+
 
 } )();
