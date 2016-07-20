415ce2552346c643f2c005caeab35483270802c2
Dylan Moberg <dylanmoberg11@gmail.com>
2016-07-20 14:19:36 -0700

    chapter 6 work

---
+++ chapter6.js
@@ -0,0 +1,54 @@
+//test code for chapter 6 of eloquent javascript
+
+function rowHeights(rows) {
+    return rows.map(function(row) {
+        return row.reduce(function(map, cell) {
+            return Math.max(max, cell.minHeight());
+        }, 0);
+    });
+}
+
+function colWidths(rows) {
+    return rows[0].map(function(_, i) {
+        return rows.reduce(function(max, row){
+            return Math.max(max, row[i].minWidth());
+        }, 0);
+    });
+}
+
+function drawTable(rows) {
+    var heights = rowHeights(rows);
+    var width = colWidths(rows);
+
+    function drawLine(blocks, lineNo) {
+        return blocks.map(function(block) {
+            return blcok[lineNo];
+        }).join(" ");
+    }
+
+    function drawRow(row, rowNum) {
+        var blocks = row.map(function(cell, colNum){
+            return cell.draw(width[colNum], heights[rowNum]);
+        });
+        return blocks[0].map(function(_, lineNo) {
+            return drawLine(blocks, lineNo);
+        }).join("\n");
+    }
+    return rows.map(drawRow).join("\n");
+}
+
+var rows = [];
+for (var i = 0; i < 5; i++) {
+   var row = [];
+   for (var j = 0; j < 5; j++) {
+     if ((j + i) % 2 == 0)
+       row.push(new TextCell("##"));
+     else
+       row.push(new TextCell("  "));
+   }
+   rows.push(row);
+}
+
+
+
+//Excersize 1
